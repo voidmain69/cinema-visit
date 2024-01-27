d6ab3461cff2345df6088c27a0d92c4ff8cfa8b2
voidmain69 <photoreznichenko@gmail.com>
2024-01-27 17:22:01 +0200

    Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,21 @@
+from app.people.cinema_staff import Cleaner
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: list,
+            cleaning_staff: Cleaner) -> None:
+
+        print(f'"{movie_name}" started in hall number {self.number}.')
+
+        for customer in customers:
+            customer.watch_movie(movie=movie_name)
+
+        print(f'"{movie_name}" ended.')
+
+        cleaning_staff.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list[dict],
+        hall_number: int,
+        cleaning_staff: str,
+        movie_name: str
+) -> None:
+
+    customers_list = [Customer(
+        name=customer["name"],
+        food=customer["food"]
+    ) for customer in customers]
+
+    hall = CinemaHall(number=hall_number)
+
+    cleaner = Cleaner(name=cleaning_staff)
+
+    for customer in customers_list:
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+
+    hall.movie_session(
+        movie_name=movie_name,
+        customers=customers_list,
+        cleaning_staff=cleaner)
